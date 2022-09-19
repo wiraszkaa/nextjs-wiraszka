86de20ef5d686c78374321cdd2506e4ca4552854
Jakub Wiraszka <WirachaPL@wp.pl>
2022-09-19 22:13:42 +0200

    UI made responsive

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -7,14 +7,14 @@ import background from "../../assets/coal.jpg";
 function Layout(props) {
   return (
     <div>
+      <div className={styles.background}>
+        <Image src={background} alt="Background" layout="responsive" />
+      </div>
       <MainNavigation />
       <div className={styles.rails}>
         <Image src={railway} alt="Railway" layout="fill" />
       </div>
       <main className={styles.main}>{props.children}</main>
-      <div className={styles.background}>
-        <Image src={background} alt="Background" layout="fill" />
-      </div>
     </div>
   );
 }
--- pages/kontakt/index.js
+++ pages/kontakt/index.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 import styles from "../../styles/Home.module.css";
 import phoneIcon from "../../assets/phone.png";
 import mailIcon from "../../assets/mail.png";
+import clockIcon from "../../assets/clock.png";
 import addressIcon from "../../assets/address.png";
 
 export default function Contact() {
@@ -18,10 +19,16 @@ export default function Contact() {
         <p className={styles.description}>+48 74 858 78 50</p>
         <Image src={mailIcon} alt="E-Mail" width={50} height={50} />
         <p className={styles.description}>m-maja1@wp.pl</p>
+        <Image src={clockIcon} alt="Zegar" width={40} height={40} />
+        <div className={styles.address}>
+          <p>pn. - pt. 8:00 - 16:00</p>
+          <p>sob. 8:00 - 15:00</p>
+        </div>
         <Image src={addressIcon} alt="Adres" width={50} height={50} />
-        <p className={styles.description}>
-          Nowy Jaworów 2, 58-140 Jaworzyna Śląska
-        </p>
+        <div className={styles.address}>
+          <p>Nowy Jaworów 2</p>
+          <p>58-140 Jaworzyna Śląska</p>
+        </div>
       </main>
     </div>
   );
